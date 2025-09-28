19bbd659f0f1d1bbe97edaa1deb2b9091c6630f4
XIOA <1327916255@qq.com>
2025-09-28 13:15:26 +0800

    appsettings

---
+++ Ava.Xioa.Entities/Models/SystemEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Ava.Xioa.Entities.Models;
+
+public class SystemEntity : BaseEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [JsonIgnore]
+    public int Id { get; set; }
+}
---
+++ Ava.Xioa.Entities/SystemDbset/SystemDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Ava.Xioa.Common.Attributes;
+using Ava.Xioa.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ava.Xioa.Entities.SystemDbset;
+
+[AutoDbContext(ServiceLifetime.Singleton)]
+public class SystemDbContext : EfDbContext.EfDbContext
+{
+    protected override string ConnectionString { get; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder,typeof(SystemEntity));
+    }
+}
