9062754420a9044ee04edd10056e060eb7f7cd52
XIOA <1327916255@qq.com>
2025-12-01 10:51:53 +0800

    TabItemDemo.axaml

---
+++ Ava.Xioa.InfrastructureModule/Views/TabItemDemo.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Ava.Xioa.Common.Attributes;
+using Ava.Xioa.Common.Const;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Ava.Xioa.InfrastructureModule.Views;
+
+[PrismRegisterForNavigation(navigationName: nameof(TabItemDemo), region: AppRegions.MainRegion)]
+public partial class TabItemDemo : UserControl
+{
+    public TabItemDemo()
+    {
+        InitializeComponent();
+    }
+}
