e40fed6094e01b9ea34a857415869e573f3ccd95
XIOA <1327916255@qq.com>
2026-06-16 16:45:31 +0800

    GlobalJsonOptions

--- Ava.Xioa.Common/Utils/GlobalJsonOptions.cs
+++ Ava.Xioa.Common/Utils/GlobalJsonOptions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Ava.Xioa.Common.Utils;
@@ -9,6 +10,7 @@ public class GlobalJsonOptions
     {
         WriteIndented = true, // 格式化输出（便于调试）
         ReferenceHandler = ReferenceHandler.IgnoreCycles, // 忽略循环引用
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping, // 忽略中文转义和符号转义
         //Converters = { new JsonStringEnumConverter() } // 枚举序列化为字符串
     };
 }
