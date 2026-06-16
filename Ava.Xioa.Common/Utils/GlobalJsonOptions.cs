cc5e6f7ef9e0244ad0379427c22371111d8d542c
XIOA <1327916255@qq.com>
2026-06-16 16:48:14 +0800

    MakeReadOnly

--- Ava.Xioa.Common/Utils/GlobalJsonOptions.cs
+++ Ava.Xioa.Common/Utils/GlobalJsonOptions.cs
@@ -6,11 +6,17 @@ namespace Ava.Xioa.Common.Utils;
 
 public class GlobalJsonOptions
 {
-    public static JsonSerializerOptions SerializeOptions { get; } = new JsonSerializerOptions
+    public readonly static JsonSerializerOptions SerializeOptions = new JsonSerializerOptions
     {
         WriteIndented = true, // 格式化输出（便于调试）
         ReferenceHandler = ReferenceHandler.IgnoreCycles, // 忽略循环引用
         Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping, // 忽略中文转义和符号转义
         //Converters = { new JsonStringEnumConverter() } // 枚举序列化为字符串
     };
+
+    // 类加载完成后锁定
+    static GlobalJsonOptions()
+    {
+        SerializeOptions.MakeReadOnly();
+    }
 }
